e832564a803760f57aaab3ad12b1cbbf72f2997e
alban voppel <alban.voppel@student.uva.nl>
2016-04-14 17:29:48 +0200

    working on design matrix

--- all_subjects_import.py
+++ all_subjects_import.py
@@ -134,66 +134,66 @@ for which_subject in subject_initials:
 		#	postFix = ['mcf','fnirted','sgtf','psc']
 		#	model = 'OG'# OG or DoG
 		#	hrf_type = 'median'
-		# session.design_matrices_for_concatenated_data(n_pixel_elements_raw = 101,n_pixel_elements_convolved=31,
-		# 					task_conditions=['PRF_01','PRF_02','PRF_03','PRF_04','PRF_05','PRF_06','PRF_07','PRF_08','PRF_09','PRF_10'])
-
 		session.design_matrices_for_concatenated_data(n_pixel_elements_raw = 101,n_pixel_elements_convolved=31,
-							task_conditions=['PRF_01'])
-			# session.setup_fit_PRF_on_concatenated_data(
-			# 	mask_file_name = mask, 
-			# 	n_jobs = n_jobs, 
-			# 	postFix = postFix, 
-			# 	plotbool = True,
-			# 	model = model,
-			# 	hrf_type = hrf_type,
-			# 	fit_on_all_data = True,
-			# 	slice_no = slice_no
-			# 	)
+							task_conditions=['PRF_01','PRF_02','PRF_03','PRF_04','PRF_05','PRF_06','PRF_07','PRF_08','PRF_09','PRF_10'])
+		#session.design_matrices_for_averaged_data()
+		#session.design_matrices_for_concatenated_data(n_pixel_elements_raw = 101,n_pixel_elements_convolved=31,
+		# 					task_conditions=['PRF_01'])
+		# # session.setup_fit_PRF_on_concatenated_data(
+		# 	mask_file_name = mask, 
+		# 	n_jobs = n_jobs, 
+		# 	postFix = postFix, 
+		# 	plotbool = True,
+		# 	model = model,
+		# 	hrf_type = hrf_type,
+		# 	fit_on_all_data = True,
+		# 	slice_no = slice_no
+		# 	)
 
-			# session.combine_seperate_slice_niftis(mask,postFix,model,task_conditions=['All'],hrf_type=hrf_type)
-			# session.convert_to_surf(mask_file = mask,postFix=postFix,model=model,hrf_type=hrf_type,depth_min=-1.0,depth_max=2.0,depth_step=0.25,task_conditions=['Fix'],sms=[0])
-			# session.combine_surfaces(mask_file = mask,postFix=postFix,model=model,hrf_type=hrf_type,depth_min=-1.0,depth_max=2.0,depth_step=0.25,task_conditions=['Fix'],sms=[0])
+		# session.combine_seperate_slice_niftis(mask,postFix,model,task_conditions=['All'],hrf_type=hrf_type)
+		# session.convert_to_surf(mask_file = mask,postFix=postFix,model=model,hrf_type=hrf_type,depth_min=-1.0,depth_max=2.0,depth_step=0.25,task_conditions=['Fix'],sms=[0])
+		# session.combine_surfaces(mask_file = mask,postFix=postFix,model=model,hrf_type=hrf_type,depth_min=-1.0,depth_max=2.0,depth_step=0.25,task_conditions=['Fix'],sms=[0])
 
-			# session.design_matrices_for_concatenated_data(n_pixel_elements_raw = 101,n_pixel_elements_convolved=31,
-			# 	change_type=change_type,run_num=run_num,task_conditions=['Fix','Color','Speed'])
-			# r_squared_threshold = 0.0005 # threshold for which voxels to fit in the non-ALL condition
-			# session.setup_fit_PRF_on_concatenated_data(
-			# 	mask_file_name = mask, 
-			# 	n_jobs = n_jobs, 
-			# 	postFix = postFix, 
-			# 	plotbool = True,
-			# 	model = model,
-			# 	hrf_type = 'median',
-			# 	fit_on_all_data = False,
-			# 	r_squared_threshold = r_squared_threshold,
-			# 	slice_no = slice_no,
-			# 	change_type = change_type,
-			# 	run_num = run_num,
-			# 	)	
+		# session.design_matrices_for_concatenated_data(n_pixel_elements_raw = 101,n_pixel_elements_convolved=31,
+		# 	change_type=change_type,run_num=run_num,task_conditions=['Fix','Color','Speed'])
+		# r_squared_threshold = 0.0005 # threshold for which voxels to fit in the non-ALL condition
+		# session.setup_fit_PRF_on_concatenated_data(
+		# 	mask_file_name = mask, 
+		# 	n_jobs = n_jobs, 
+		# 	postFix = postFix, 
+		# 	plotbool = True,
+		# 	model = model,
+		# 	hrf_type = 'median',
+		# 	fit_on_all_data = False,
+		# 	r_squared_threshold = r_squared_threshold,
+		# 	slice_no = slice_no,
+		# 	change_type = change_type,
+		# 	run_num = run_num,
+		# 	)	
 
-			# session.combine_seperate_slice_niftis(mask,postFix,model,task_conditions = ['Fix','Color','Speed'],hrf_type=hrf_type)
+		# session.combine_seperate_slice_niftis(mask,postFix,model,task_conditions = ['Fix','Color','Speed'],hrf_type=hrf_type)
 
-			# CV:
-			# session.combine_seperate_slice_niftis(mask,postFix,model,task_conditions = ['Fix','Color','Speed'],hrf_type=hrf_type,change_type='leave_one_out')
-			# session.cross_predictions_concatenated_data(mask,postFix,model,hrf_type,n_jobs,run_num)
-			# session.combine_predictions_concatenated_data(hrf_type,model,postFix,mask)
+		# CV:
+		# session.combine_seperate_slice_niftis(mask,postFix,model,task_conditions = ['Fix','Color','Speed'],hrf_type=hrf_type,change_type='leave_one_out')
+		# session.cross_predictions_concatenated_data(mask,postFix,model,hrf_type,n_jobs,run_num)
+		# session.combine_predictions_concatenated_data(hrf_type,model,postFix,mask)
 
 
-			# session.compare_fits(mask)
-			
-			# task_conditions = ['Stim']#['All','Fix','Color','Speed']
-			# session.mask_stats_to_hdf(mask_file = mask , postFix = postFix, task_conditions = task_conditions,model=model,hrf_type=hrf_type)
-			# session.fit_diagnostics(task_conditions = task_conditions, maskfile=mask, ecc_thresh=[0.0,7.0],model=model, 
-			# 						hists = False, 
-			# 						eccen_surf = True, 
-			# 						r_squared_threshold = 0.2,
-			# 						condition_comparison_plots = False, 
-			# 						correlation_of_ecc_fwhm_diff_plot = False,
-			# 						polar_plot = False,
-			# 						simple_size_plot = False,
-			# 						polar_imshow=False,
-			# 						fba_attent_corr_plots=False,
-			# 						postFix=postFix)
+		# session.compare_fits(mask)
+		
+		# task_conditions = ['Stim']#['All','Fix','Color','Speed']
+		# session.mask_stats_to_hdf(mask_file = mask , postFix = postFix, task_conditions = task_conditions,model=model,hrf_type=hrf_type)
+		# session.fit_diagnostics(task_conditions = task_conditions, maskfile=mask, ecc_thresh=[0.0,7.0],model=model, 
+		# 						hists = False, 
+		# 						eccen_surf = True, 
+		# 						r_squared_threshold = 0.2,
+		# 						condition_comparison_plots = False, 
+		# 						correlation_of_ecc_fwhm_diff_plot = False,
+		# 						polar_plot = False,
+		# 						simple_size_plot = False,
+		# 						polar_imshow=False,
+		# 						fba_attent_corr_plots=False,
+		# 						postFix=postFix)
 
 	# ----------------------
 	# Initialise session   -
